a8049546276ccaf9cd3448692326d74c7a8e4fb4
KapitanZbychu <nordray@gmail.com>
2018-10-20 22:57:30 +0200

    Sprawdzanie pól, rysowanie X/O

--- Game/src/game/Gra.java
+++ Game/src/game/Gra.java
@@ -22,10 +22,47 @@ public class Gra {
         } else if (pole1 == 2) {
             Graphics.drawO(szerokosc, x1, y1);
         }
-        if (pole1 == 1){
+        if (pole2 == 1){
             Graphics.drawX(szerokosc, x1+szerokosc, y1);
-        } else if (pole1 == 2) {
+        } else if (pole2 == 2) {
             Graphics.drawO(szerokosc, x1+szerokosc, y1);
+        }
+        if (pole3 == 1){
+            Graphics.drawX(szerokosc, x1+szerokosc*2, y1);
+        } else if (pole3 == 2) {
+            Graphics.drawO(szerokosc, x1+szerokosc*2, y1);
+        }
+        
+        if (pole4 == 1){
+            Graphics.drawX(szerokosc, x1, y1-szerokosc);
+        } else if (pole4 == 2) {
+            Graphics.drawO(szerokosc, x1, y1-szerokosc);
+        }
+        if (pole5 == 1){
+            Graphics.drawX(szerokosc, x1+szerokosc, y1-szerokosc);
+        } else if (pole5 == 2) {
+            Graphics.drawO(szerokosc, x1+szerokosc, y1-szerokosc);
+        }
+        if (pole6 == 1){
+            Graphics.drawX(szerokosc, x1+szerokosc*2, y1-szerokosc);
+        } else if (pole6 == 2) {
+            Graphics.drawO(szerokosc, x1+szerokosc*2, y1-szerokosc);
+        }
+        
+        if (pole7 == 1){
+            Graphics.drawX(szerokosc, x1, y1-szerokosc*2);
+        } else if (pole7 == 2) {
+            Graphics.drawO(szerokosc, x1, y1-szerokosc*2);
+        }
+        if (pole8 == 1){
+            Graphics.drawX(szerokosc, x1+szerokosc, y1-szerokosc*2);
+        } else if (pole8 == 2) {
+            Graphics.drawO(szerokosc, x1+szerokosc, y1-szerokosc*2);
+        }
+        if (pole9 == 1){
+            Graphics.drawX(szerokosc, x1+szerokosc*2, y1-szerokosc*2);
+        } else if (pole9 == 2) {
+            Graphics.drawO(szerokosc, x1+szerokosc*2, y1+-szerokosc*2);
         }
               
     }
